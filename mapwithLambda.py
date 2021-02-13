88ee7649925ffdb9173d93130aa600671a1e4b54
revanthpalla <revanth4902@gmail.com>
2021-02-13 18:52:31 +0530

    Added one more function in mapwithLambda.py

--- mapwithLambda.py
+++ mapwithLambda.py
@@ -1,14 +1,19 @@
+def square(x):
+  return x*x
+
 nums = [1,2,3,4,45]
 sqlist = map(lambda x:x*x,nums)
 print(list(sqlist))
 
-#also we can write same like this
-
-def square(x):
-  return x*x
+# Another Method
 nums = [1,2,3,4,45]
 sqlist = map(square,nums)
 print(next(sqlist)) #1
 print(next(sqlist)) #4
 print(next(sqlist)) #9
 print(next(sqlist)) #16
+
+# Making Changes
+square = lambda x: x**2
+square_of_list = list(map(square,[1,2,3,4]))
+print(square_of_list)
